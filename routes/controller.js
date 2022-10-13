94cb5e12adda32ec6383fc509118234c8f764db5
정현정 <33312138+net7281@users.noreply.github.com>
2022-10-13 13:51:40 +0900

    컨트롤러 쿠키 부분수정
    쿠키를 함수로 생성하니까 값이 안들어간다ㅜㅜ 일단 밖으로 뺐다
    
    req.headers.cookie 이렇게 if를 짜니까 안에 톰캣이 만든 쿠키가 들어있어서 내 생각에는 안들어오는거 같다

--- routes/controller.js
+++ routes/controller.js
@@ -6,7 +6,7 @@ var fs = require('fs') // file system 이라고 해서 파일에 대한 전반�
 //     res.end("Hello World")
 // }
 
-const {fortunePick} = require("../models/contentDAO");
+const fortunePick = require("../models/contentDAO");
 
 ///fortunePick/////////////////////////////////////////
 
@@ -18,62 +18,75 @@ function randomKey(){
 ///////////////////////////////////////////////////////
 
 ///cookie//////////////////////////////////////////////
-function cookieMake(req,res,key){
+// function cookieMake(req,res,key){
 
-    //오늘 날짜 저장
-    let today = new Date();
-    TodayDate = today.toLocaleDateString();
+//     //오늘 날짜 저장
+//     let today = new Date();
+//     TodayDate = today.toLocaleDateString();
 
-    //res.setHeader > 값 하나만 전달
-    //res.writeHead > 값 여러개 전달
-
+//     //res.setHeader > 값 하나만 전달
+//     //res.writeHead > 값 여러개 전달
     
-    if(req.headers.cookie==undefined){
-        res.writeHead(200, { 
-            'Content-Type': 'text/html; charset=utf-8',
-            'Set-Cookie': [
-
-            //이틀후 쿠키 삭제
-            // 억음부호 ` (물결위치) = ${}를 쓸때 쓴다
-            'key='+ key+';'+`Max-Age=${50}`, 
-            'created_date=' + TodayDate+';'+`Max-Age=${50}`
-            // 60*60*24*2 = 이틀
-            ]
-        })
-        console.log("쿠키생성")
-    }else{
-        res.writeHead(200, { 
-        'Content-Type': 'text/html; charset=utf-8'})
-        console.log("쿠키사용")
-    }
+//     if(req.headers.cookie==undefined){
+//         res.writeHead(200, { 
+//             'Content-Type': 'text/html; charset=utf-8',
+//             'Set-Cookie': [
+//             //이틀후 쿠키 삭제
+//             // 억음부호 ` (물결위치) = ${}를 쓸때 쓴다
+//             'key='+ key+';'+`Max-Age=${50}`, 
+//             'created_date=' + TodayDate+';'+`Max-Age=${50}`
+//             // 60*60*24*2 = 이틀
+//             ]
+//         })
+//         console.log("쿠키생성")
+//     }else{
+//         res.writeHead(200, { 
+//         'Content-Type': 'text/html; charset=utf-8'})
+//         console.log("쿠키사용")
+//     }
     
     
-    //res.setHeader('Set-Cookie', `token=1`);
-    //res.send(key+" "+content)
-    console.log(req.headers.cookie);
-    //console.log(cookies.created_date);
-    res.end(key+" ")
-}
+//     //res.setHeader('Set-Cookie', `token=1`);
+//     //res.send(key+" "+content)
+//     console.log(req.headers.cookie);
+//     //console.log(cookies.created_date);
+//     res.end(key+" ")
+// }
 ////////////////////////////////////////////////////
 
 
 
-
+// /main을 호출하면 ~
 exports.mainView = function (req, res) {
-// utf8 은 인코딩 부분입니다.
-    
          //랜덤키값 저장
         const key = randomKey();
-
-        cookieMake(req,res,key);
-        const pick = new fortunePick(key).catch(console.dir);
-
+        //cookieMake(req,res,key);
+        let today = new Date();
+         TodayDate = today.toLocaleDateString();
+        if(req.headers.cookie==undefined){
+            res.writeHead(200, { 
+                'Content-Type': 'text/html; charset=utf-8',
+                'Set-Cookie': [
+                //이틀후 쿠키 삭제
+                // 억음부호 ` (물결위치) = ${}를 쓸때 쓴다
+                'key='+ key+';'+`Max-Age=${50}`, 
+                'created_date=' + TodayDate+';'+`Max-Age=${50}`
+                // 60*60*24*2 = 이틀
+                ]
+            });
+            console.log("쿠키생성")
+         }else{
+            res.writeHead(200, { 
+            'Content-Type': 'text/html; charset=utf-8'})
+            console.log("쿠키사용")
+        };
+        //const pick = new fortunePick(key).catch(console.dir);
+        res.end(key+" ")
         console.log(req.headers.cookie);
-
-
-fs.readFile("./views/index.html", "utf8", function (err,buf) {
+        
+// utf8 은 인코딩 부분입니다.
+    fs.readFile("./views/index.html", "utf8", function (err,buf) {
         res.end(buf);
-
     })
 }
 
